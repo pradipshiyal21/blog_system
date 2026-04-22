3c30b634b691d6d519ab7717f72ccf366d827238
Pradip Shiyal <pradip.shiyal@mail.nitsan.ai>
2026-04-22 18:40:40 +0530

    Comment out blog module configuration in Backend

--- Configuration/Backend/Modules.php
+++ Configuration/Backend/Modules.php
@@ -1,15 +1,15 @@
 <?php
 return [
-    'blog_blogkey' => [
-        'parent' => 'web',
-        'position' => [],
-        'access' => 'user,group',
-        'iconIdentifier' => 'blog-module-blogkey',
-        'labels' => 'LLL:EXT:blog/Resources/Private/Language/locallang_blogkey.xlf',
-        'extensionName' => 'Blog',
-        'controllerActions' => [
-            \MyVendor\Blog\Controller\PostController::class => ['index', 'list', 'show', 'new', 'create', 'edit', 'update', 'delete'],
+    // 'blog_blogkey' => [
+    //     'parent' => 'web',
+    //     'position' => [],
+    //     'access' => 'user,group',
+    //     'iconIdentifier' => 'blog-module-blogkey',
+    //     'labels' => 'LLL:EXT:blog/Resources/Private/Language/locallang_blogkey.xlf',
+    //     'extensionName' => 'Blog',
+    //     'controllerActions' => [
+    //         \MyVendor\Blog\Controller\PostController::class => ['index', 'list', 'show', 'new', 'create', 'edit', 'update', 'delete'],
             
-        ],
-    ],
+    //     ],
+    // ],
 ];
