fe2d4798eceedead2663903dba64ac477730663c
Pradip Shiyal <pradip.shiyal@mail.nitsan.ai>
2026-05-04 17:48:52 +0530

    extend core features using Xclass & added one button in doc header

---
+++ Classes/Xclass/AddDocHeaderButton.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace MyVendor\Blog\Xclass;
+
+use TYPO3\CMS\Backend\Template\Components\ButtonBar;
+use TYPO3\CMS\Backend\Template\Components\Buttons\LinkButton;
+use TYPO3\CMS\Core\Imaging\Icon;
+use TYPO3\CMS\Core\Imaging\IconFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class AddDocHeaderButton extends ButtonBar
+{
+    public function getButtons(): array
+    {
+        $buttons = parent::getButtons();
+        
+        $iconFactory = GeneralUtility::makeInstance(IconFactory::class);
+        $myButton = GeneralUtility::makeInstance(LinkButton::class)
+            ->setHref('#')
+            ->setTitle('My Button')
+            ->setShowLabelText(true)
+            ->setIcon($iconFactory->getIcon('actions-add', Icon::SIZE_SMALL));
+
+        $buttons['left'][0][] = $myButton;
+        return $buttons;
+    }
+}
